264d3d630aa5b84f911c7014bcb70b7e219be17e
qod <crickerbug@gmail.com>
2021-08-10 20:31:19 -0400

    1.2

--- truffle-config.js
+++ truffle-config.js
@@ -28,10 +28,10 @@ module.exports = {
             gas: 8000000,
             gasPrice: 12000000000, // 12 Gwei
         },
-        bsctestnet: {
-            provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-            network_id: 97,
-            gas: 28765632,
+        bsc: {
+            provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+            network_id: 56,
+            gas: 3300000,
             gasPrice: 10000000000, // 10 Gwei
         },
     },
